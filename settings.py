3b9a6a6c65883b2bf86f94b788966906e32eb2a8
Merijn <merijn.bertels@gmail.com>
2010-08-27 19:11:12 +0200

    CSS for login

--- settings.py
+++ settings.py
@@ -1,3 +1,6 @@
+import os
+
+BASE_PATH = os.path.abspath(os.path.dirname(__file__))
 # Django settings for bramedaerp project.
 
 DEBUG = True
@@ -27,7 +30,7 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Europa/Amsterdam'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -81,6 +84,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+	os.path.join(BASE_PATH,'templates')
 )
 
 INSTALLED_APPS = (
