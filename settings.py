d26536b692a43b1041b72e30f90900ca75fe5574
Merijn <merijn.bertels@gmail.com>
2010-09-04 15:46:52 +0200

    Error middleware

--- settings.py
+++ settings.py
@@ -76,7 +76,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-	'jogging.middleware.LoggingMiddleware',
+	'brameda.jogging.middleware.LoggingMiddleware',
 )
 
 ROOT_URLCONF = 'brameda.urls'
